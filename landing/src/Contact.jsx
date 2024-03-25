3d82658fa2b785a87ec0be314d6f07e2da0b5bf5
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-25 16:25:58 -0300

    alinhando com a bel

--- landing/src/Contact.jsx
+++ landing/src/Contact.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
@@ -11,11 +13,35 @@ import style from "./Contact.module.css"
 export const Contact = () => {
     const geoData = ({lat: -25.3718243, lng: -49.2161051})
 
+    const defaultPhoneNumber = '5541999999999'
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    const handleWhatsAppMessage = () => {
+        const {name, email, message} = formData;
+        
+        const whatsappUrl = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
+        Nome:%20${name}%0D%0A
+        Email:%20${email}%0D%0A
+        Mensagem:%20${message}`;
+
+        window.open(whatsappUrl, '_blank');
+    }
+
     return(
-        <div>
+        <div className={style['wrap-all-contact']}>
             <Menu/>
-            <h1>Contato</h1>
-            <div>
+            <h1 className={style.wrapH1}>Contato</h1>
+            <div className={style.wrapTwoDivs}>
                 <div className={style.wrapMap}>
                     <h2>Mapa</h2>
                     <MapContainer center={[geoData.lat, geoData.lng]} zoom={17} scrollWheelZoom={false} style={{width: "100%", height: "100%"}}>
@@ -34,7 +60,19 @@ export const Contact = () => {
                 <div>
                     <h2>Zap</h2>
                     <div>
-                        
+                        <div>
+                            <label htmlFor="name">Nome</label>
+                            <input type="text" id='name' name='name' value={formData.name} onChange={handleChange} required/>
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id='email' name='email' value={formData.email} onChange={handleChange} required/>
+                        </div>
+                        <div>
+                            <label htmlFor="message">Mensagem</label>
+                            <textarea id='message' name='message' value={formData.message} onChange={handleChange} required></textarea>
+                        </div>
+                        <button onClick={handleWhatsAppMessage}>Enviar mensagem</button>
                     </div>
                 </div>
             </div>
